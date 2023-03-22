2379f7c4afcd36d74c2344102653abfe2a433f12
elaaasri <anas_elasri@outlook.fr>
2023-03-22 18:02:03 +0000

    .

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
+// Main Container : 
 let mainDiv = document.createElement("div");
 mainDiv.id = "Main";
 document.body.appendChild(mainDiv);
-
-let containerDiv = document.createElement("div");
-containerDiv.className = "Grid-Container";
-mainDiv.appendChild(containerDiv);
-
+// Grid Container : 
+let gridContainer = document.createElement("div");
+gridContainer.id = "Grid-Container";
+mainDiv.appendChild(gridContainer);
+// The Start Grid 16*16 : 
 for (let i = 0; i < 256; i++) {
-    var squareDivs = document.createElement("div");
+    let squareDivs = document.createElement("div");
     squareDivs.className = "Squares";
-    squareDivs.textContent = "box";
-    containerDiv.appendChild(squareDivs);
+    gridContainer.appendChild(squareDivs);
 
     squareDivs.addEventListener("mouseover",function() {
-        console.log("zbe");
         this.style.cssText = "background-color: blue";
-    });
+    }); 
 };
 
+// Button To Change Number Of Squares : 
 let Button = document.createElement("button");
 Button.className = "Button";
 Button.textContent = "Button";
 mainDiv.prepend(Button);
 
+Button.addEventListener("click", () => {
+    let numbersOfSquares = prompt("Enter Number Of Squares : ");
+    let output = numbersOfSquares * numbersOfSquares;
 
+    // Setting The Limit Of The Number Of Squares : 
+    if (numbersOfSquares > 100) {
+        return numbersOfSquares;
+    }
+    // Removing The Squares :
+    let allSquares = document.querySelectorAll(".Squares");
+    allSquares.forEach(square => {
+        square.remove();
+    });
+    // Generating The New Grid :
+    for (let i = 0; i < output; i++) {
+        let squareDivs = document.createElement("div");
+        squareDivs.className = "Squares";
+        gridContainer.appendChild(squareDivs);
+        // Changing The Color When Hovering : 
+        squareDivs.addEventListener("mouseover",function() {
+            this.style.cssText = "background-color: blue";
+        }); 
+    };
+});
