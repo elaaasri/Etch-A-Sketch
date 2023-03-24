36bf4427f8810ad26fb722a490c0302a4fb508ce
elaaasri <anas_elasri@outlook.fr>
2023-03-24 16:31:29 +0000

    .

--- script.js
+++ script.js
@@ -1,51 +1,128 @@
 // Main Container : 
 let mainDiv = document.createElement("div");
-mainDiv.id = "Main";
+mainDiv.className = "main";
 document.body.appendChild(mainDiv);
 // Grid Container : 
 let gridContainer = document.createElement("div");
-gridContainer.id = "Grid-Container";
+gridContainer.className = "grid-container";
 mainDiv.appendChild(gridContainer);
-// The Start Grid 16*16 :
-for (let i = 0; i < 256; i++) {
-    let squareDivs = document.createElement("div");
-    squareDivs.className = "Squares";
-    gridContainer.appendChild(squareDivs);
-
-    squareDivs.addEventListener("mouseover",function() {
-        this.style.cssText = "background-color: blue";
-    }); 
-};
-// Button To Change Number Of Squares : 
-let Button = document.createElement("button");
-Button.className = "Button";
-Button.textContent = "Button";
-mainDiv.prepend(Button);
-
-Button.addEventListener("click", () => {
-    let numbersOfSquares = prompt("Enter Number Of Squares : ");
-    let output = numbersOfSquares * numbersOfSquares;
-    // Setting The Limit Of The Number Of Squares : 
-    if (numbersOfSquares > 64) {
-        alert("Max Number Of Squares Is 100!")
-        return numbersOfSquares;
-    };
-    // Removing The Squares :
-    let allSquares = document.querySelectorAll(".Squares");
-    allSquares.forEach(square => {
+// Settings Container :
+let settingsContainer = document.createElement("div");
+settingsContainer.className = "settings";
+mainDiv.prepend(settingsContainer);
+// Slider Button :
+let slider = document.querySelector(".slider");
+let numbersOfSquares = document.querySelector("p");
+settingsContainer.prepend(slider);
+// RainBow Button :
+let rainBow = document.createElement("button");
+rainBow.className = "random-color";
+rainBow.textContent = "RainBow Mode";
+settingsContainer.appendChild(rainBow);
+// Color Mode Button : 
+let colorMode = document.createElement("button");
+colorMode.className = "color-mode";
+colorMode.textContent = "Color Mode";
+settingsContainer.appendChild(colorMode);
+ // Removing The Squares :
+function removeSquares(){
+        let allSquares = document.querySelectorAll(".squares");
+        allSquares.forEach(square => {
         square.remove();
     });
-    // Generating The New Grid :
+};
+// Generating The New Grid :
+slider.addEventListener("mouseup",() => {
+    // Deleting The Squares Before Generating New Grid : 
+    removeSquares();
+    // New Grid : 
+    let output = numbersOfSquares.textContent * numbersOfSquares.textContent;
+    let squareDivs;
     for (let i = 0; i < output; i++) {
-        let squareDivs = document.createElement("div");
-        squareDivs.className = "Squares";
+        squareDivs = document.createElement("div");
+        squareDivs.className = "squares";
         gridContainer.appendChild(squareDivs);
         gridContainer.style.display = "grid";
-        gridContainer.style.gridTemplateColumns = `repeat(${numbersOfSquares}, 1fr)`;
-        gridContainer.style.gridTemplateRows = `repeat(${numbersOfSquares}, 1fr)`;
-        // Changing The Color When Hovering :
-        squareDivs.addEventListener("mouseover",function() {
-            this.style.cssText = "background-color: blue";
-        }); 
+        gridContainer.style.gridTemplateColumns = `repeat(${numbersOfSquares.textContent}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${numbersOfSquares.textContent}, 1fr)`;
     };
 });
+// Color Mode When Hovering :
+colorMode.addEventListener("click", function() {
+    let allSquares = document.querySelectorAll(".squares");
+    allSquares.forEach(square => square.addEventListener("mouseover",() => {
+        square.style.cssText = "background-color: black";
+    }));
+});
+// Random Color When Hovering : 
+rainBow.addEventListener("click",function(){
+    let allSquares = document.querySelectorAll(".squares")
+    allSquares.forEach(square => square.addEventListener("mouseover",() => {
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        square.style.backgroundColor = "#" + randomColor;
+    }));    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function newGrid() {
+//     // Deleting The Squares Before Generating New Grid : 
+//     removeSquares();
+//     // Generating The New Grid :
+//     let output = numbersOfSquares.textContent * numbersOfSquares.textContent;
+//     for (let i = 0; i < output; i++) {
+//     let squareDivs = document.createElement("div");
+//     squareDivs.className = "squares";
+//     gridContainer.appendChild(squareDivs);
+//     gridContainer.style.display = "grid";
+//     gridContainer.style.gridTemplateColumns = `repeat(${numbersOfSquares.textContent}, 1fr)`;
+//     gridContainer.style.gridTemplateRows = `repeat(${numbersOfSquares.textContent}, 1fr)`;
+//     // Color Mode When Hovering :
+//         colorMode.addEventListener("click", function() {
+//             squareDivs.addEventListener("mouseover",function() {
+//                 this.style.cssText = "background-color: black";
+//             });
+//         })
+//     // Random Color When Hovering : 
+//         rainBow.addEventListener("click",function(){
+//             squareDivs.addEventListener("mouseover",() => {
+//                 const zbe = Math.floor(Math.random()*16777215).toString(16);
+//                 squareDivs.style.backgroundColor = "#" + zbe;
+//             });
+//         })
+//     };
+// };
+// newGrid()
+// slider.addEventListener("mouseup", newGrid);
+
+
+
+
