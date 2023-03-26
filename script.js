1ae8a27cafa429021899702993f84b149e4bbaa4
elaaasri <anas_elasri@outlook.fr>
2023-03-26 14:25:06 +0000

    .

--- script.js
+++ script.js
@@ -1,45 +1,13 @@
-    // Main Container : 
-let mainDiv = document.createElement("div");
-mainDiv.className = "main";
-document.body.appendChild(mainDiv);
-    // Grid Container : 
-let gridContainer = document.createElement("div");
-gridContainer.className = "grid-container";
-mainDiv.appendChild(gridContainer);
-    // Settings Container :
-let settingsContainer = document.createElement("div");
-settingsContainer.className = "settings";
-mainDiv.prepend(settingsContainer);
-    // Slider Button :
+let mainDiv = document.querySelector(".main");
+let gridContainer = document.querySelector(".grid-container");
+let settingsContainer = document.querySelector(".settings");
 let slider = document.querySelector(".slider");
 let numbersOfSquares = document.querySelector("p");
-settingsContainer.prepend(slider);
-    // Color Mode Button : 
-let colorMode = document.createElement("button");
-colorMode.className = "color-mode";
-colorMode.textContent = "Color Mode";
-let colorInput = document.createElement("input");
-colorInput.className = "color-input";
-colorInput.type = "color";
-settingsContainer.appendChild(colorInput);
-settingsContainer.appendChild(colorMode);
-    // RainBow Button :
-let rainBow = document.createElement("button");
-rainBow.className = "random-color";
-rainBow.textContent = "RainBow Mode";
-settingsContainer.appendChild(rainBow);
-let eraserButton = document.createElement("button");
-eraserButton.className = "eraser-button";
-eraserButton.textContent = "Erazer";
-settingsContainer.appendChild(eraserButton);
-    // Clear Button : 
-let clearButton = document.createElement("button");
-clearButton.className ="clear-button";
-clearButton.textContent = "Clear";
-settingsContainer.appendChild(clearButton);
-    // Footer  Appending :
-let footer = document.querySelector(".footer");
-document.body.append(footer);
+let colorMode = document.querySelector(".color-mode");
+let colorInput = document.querySelector(".color-input");
+let rainBow = document.querySelector(".random-color");
+let eraserButton = document.querySelector(".eraser-button");
+let clearButton = document.querySelector(".clear-button");
 
     // Removing The Squares :
 function removeSquares(){
@@ -53,13 +21,13 @@ function removeSquares(){
 function newGrid(){
         // Deleting The Squares Before Generating New Grid : 
     removeSquares();
+
         // New Grid : 
     let sliderOutput = numbersOfSquares.textContent * numbersOfSquares.textContent;
     for (let i = 0; i < sliderOutput; i++) {
-            let squareDivs = document.createElement("div");
+        let squareDivs = document.createElement("div");
             squareDivs.className = "squares";
             gridContainer.appendChild(squareDivs);
-            gridContainer.style.display = "grid";
             gridContainer.style.gridTemplateColumns = `repeat(${numbersOfSquares.textContent}, 1fr)`;
             gridContainer.style.gridTemplateRows = `repeat(${numbersOfSquares.textContent}, 1fr)`;
         };
@@ -68,34 +36,40 @@ newGrid();
 
     // Slider Button to Generate new Grid : 
 slider.addEventListener("mouseup",newGrid);
-    // Selecting All The Squares :
-let allSquares = document.querySelectorAll(".squares");
 
     // Random Color : 
 function getRandomRGB() {
     let red = Math.floor(Math.random() * 255);
     let green = Math.floor(Math.random() * 255);
     let blue = Math.floor(Math.random() * 255);
-
+    
     return `rgb(${red},${green},${blue})`;
-}
+};
+
 rainBow.addEventListener("click",() => {
+    let allSquares = document.querySelectorAll(".squares");
     allSquares.forEach(square => square.addEventListener("mouseover",() => {
         square.style.backgroundColor = `${getRandomRGB()}`;
     }));
 }); 
+
     // Eraser Button Event :
-    eraserButton.addEventListener("click",() => {
-        allSquares.forEach(square => square.addEventListener("mouseover",() => {
-            square.style.cssText = "";
-        }));
-    });
+eraserButton.addEventListener("click",() => {
+    let allSquares = document.querySelectorAll(".squares");
+    allSquares.forEach(square => square.addEventListener("mouseover",() => {
+        square.style.cssText = "";
+    }));
+});
+
     // Clear Button Event :
 clearButton.addEventListener("click", () => {
+    let allSquares = document.querySelectorAll(".squares");
     allSquares.forEach(square => square.style.cssText = "");
 });
+
     // Color Mode Event :
 colorMode.addEventListener("click", () => {
+    let allSquares = document.querySelectorAll(".squares");
     allSquares.forEach(square => square.addEventListener("mouseover",() => {
         square.style.cssText = `background-color: ${colorInput.value}`;
     }));
